5fa3a653962f95d75be18bdd87f66b789ed2ad53
Thomas Ng <thomas.ng@nikhef.nl>
2025-11-21 16:02:59 +0100

    Refactor type hints in MALA and RQSpline_MALA classes to not use union syntax for step size

--- src/flowMC/resource/kernel/MALA.py
+++ src/flowMC/resource/kernel/MALA.py
@@ -2,7 +2,7 @@ import jax
 import jax.numpy as jnp
 from jax.scipy.stats import multivariate_normal
 from jaxtyping import Array, Bool, Float, Int, PRNGKeyArray, PyTree
-from typing import Callable, Union
+from typing import Callable
 
 from flowMC.resource.logPDF import LogPDF
 from flowMC.resource.kernel.base import ProposalBase
@@ -18,7 +18,7 @@ class MALA(ProposalBase):
 
     def __init__(
         self,
-        step_size: Union[float, Float[Array, " n_dim n_dim"]],
+        step_size: Float | Float[Array, " n_dim n_dim"],
     ):
         super().__init__()
         self.step_size = step_size
@@ -48,12 +48,12 @@ class MALA(ProposalBase):
 
         def body(
             carry: tuple[
-                Float[Array, " n_dim"], Union[float, Float[Array, " n_dim n_dim"]], dict
+                Float[Array, " n_dim"], Float | Float[Array, " n_dim n_dim"], dict
             ],
             this_key: PRNGKeyArray,
         ) -> tuple[
             tuple[
-                Float[Array, " n_dim"], Union[float, Float[Array, " n_dim n_dim"]], dict
+                Float[Array, " n_dim"], Float | Float[Array, " n_dim n_dim"], dict
             ],
             tuple[Float[Array, " n_dim"], Float[Array, "1"], Float[Array, " n_dim"]],
         ]:
@@ -69,7 +69,7 @@ class MALA(ProposalBase):
 
         key1, key2 = jax.random.split(rng_key)
 
-        dt: Union[float, Float[Array, " n_dim n_dim"]] = self.step_size
+        dt: Float | Float[Array, " n_dim n_dim"] = self.step_size
         dt2 = dt * dt
 
         _, (proposal, logprob, d_logprob) = jax.lax.scan(
