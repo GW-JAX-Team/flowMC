fix: update step size calculation in GaussianRandomWalk, HMC, and MALA classes

src/flowMC/resource/kernel/Gaussian_random_walk.py
@@ -82,7 +82,7 @@ class GaussianRandomWalk(ProposalBase):
             A new GaussianRandomWalk instance with updated step_size.
         """
         diff = acceptance_rate - target_rate
-        new_step_size = self.step_size * jnp.exp(self.ADAPTATION_RATE * diff)
+        new_step_size = self.step_size * (1.0 + self.ADAPTATION_RATE * diff)
         return tree_at(lambda k: k.step_size, self, new_step_size)
 
     def print_parameters(self):

src/flowMC/resource/kernel/MALA.py
@@ -115,7 +115,7 @@ class MALA(ProposalBase):
             A new MALA instance with updated step_size.
         """
         diff = acceptance_rate - target_rate
-        new_step_size = self.step_size * jnp.exp(self.ADAPTATION_RATE * diff)
+        new_step_size = self.step_size * (1.0 + self.ADAPTATION_RATE * diff)
         return tree_at(lambda k: k.step_size, self, new_step_size)
 
     def print_parameters(self):